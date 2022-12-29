8f11a4bdfdb01365c637a5a596ac528c55295d3f
maleal <mariolealfuentes@gmail.com>
2022-12-29 17:15:05 -0300

    Add Polymorphis-Interfaces project to the solution.

---
+++ Polymorphism/Polymorphis-Interfaces/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Polymorphis_Interfaces
+{
+    public class Program
+    {
+
+
+        public static void Main()
+        {
+            IFile file1 = new FileInfo();
+
+            FileInfo file2 = new FileInfo();
+            UsbInfo usb2 = new UsbInfo();
+
+            file1.ReadFile();
+            file1.WriteFile("file content");
+
+            file2.ReadFile();
+            file2.WriteFile("file content");
+
+            Caller(file2);
+            Caller(usb2);
+
+        }
+
+        public static void Caller(IFile fce)
+        {
+            fce.WriteFile("-Caller-");
+        }
+    }
+
+    public interface IFile
+    {
+        void ReadFile();
+        void WriteFile(string text);
+    }
+
+    class FileInfo : IFile
+    {
+        public void ReadFile()
+        {
+            Console.WriteLine("Reading File");
+        }
+
+        public void WriteFile(string text)
+        {
+            Console.WriteLine("Writing to File:{0}", text);
+        }
+    }
+
+    class UsbInfo : IFile
+    {
+        public void ReadFile()
+        {
+            Console.WriteLine("Reading File");
+        }
+
+        public void WriteFile(string text)
+        {
+            Console.WriteLine("Writing to Usb:{0}", text);
+        }
+    }
+
+}
